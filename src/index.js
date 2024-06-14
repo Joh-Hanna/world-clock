01a657e55f4f9c4ea38a71a4070257d303f85432
Joh-Hanna <Johannaruhm@web.de>
2024-06-15 00:42:49 +0200

    added another city

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 function updateTime() {
 
 
-let londonElement = document.querySelector("#london");
-if (londonElement) {
-let londonTimeElement = londonElement.querySelector(".time");
-let londonTime = moment.tz("Europe/London");
-let londonDateElement = londonElement.querySelector(".date");
-londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
-londonDateElement.innerHTML = londonTime.format("MMM Do YYYY");
+    let londonElement = document.querySelector("#london");
+    if (londonElement) {
+    let londonTimeElement = londonElement.querySelector(".time");
+    let londonTime = moment.tz("Europe/London");
+    let londonDateElement = londonElement.querySelector(".date");
+    londonTimeElement.innerHTML = londonTime.format("h:mm:ss [<small>]A[</small>]");
+    londonDateElement.innerHTML = londonTime.format("MMM Do YYYY");
+    
+    }
+    
+    let mauritiusElement = document.querySelector("#mauritius");
+    if (mauritiusElement) {
+    let mauritiusTimeElement = mauritiusElement.querySelector(".time");
+    let mauritiusTime = moment.tz("Indian/Mauritius");
+    let mauritiusDateElement = mauritiusElement.querySelector(".date");
+    mauritiusTimeElement.innerHTML = mauritiusTime.format("h:mm:ss [<small>]A[</small>]");
+    mauritiusDateElement.innerHTML = mauritiusTime.format("MMM Do YYYY");
+    
+    }
+    let pragueElement = document.querySelector("#prague");
+    if (pragueElement) {
+    let pragueTimeElement = pragueElement.querySelector(".time");
+    let pragueTime = moment.tz("Europe/Prague");
+    let pragueDateElement = pragueElement.querySelector(".date");
+    pragueTimeElement.innerHTML = `${pragueTime.format("h:mm:ss [<small>]A[</small>]")}`;
+    pragueDateElement.innerHTML = `${pragueTime.format("MMM Do YYYY")}`;
+    
+    }
+    }
 
-}
-let pragueElement = document.querySelector("#prague");
-if (pragueElement) {
-let pragueTimeElement = pragueElement.querySelector(".time");
-let pragueTime = moment.tz("Europe/Prague");
-let pragueDateElement = pragueElement.querySelector(".date");
-pragueTimeElement.innerHTML = `${pragueTime.format("h:mm:ss [<small>]A[</small>]")}`;
-pragueDateElement.innerHTML = `${pragueTime.format("MMM Do YYYY")}`;
 
-}
-}
 function updateCity(event) {
+
+    
     
     let cityTimezone = event.target.value;
     if (cityTimezone === "current") {
@@ -34,21 +48,19 @@ function updateCity(event) {
         <div class="city">
             <div>
                 <h2>${cityName}</h2>
+
     <div class="date">${cityTime.format("MMM Do YYYY")}</div></div>
     <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small></div></div>
    <a href="/">All cities</a>
 `;
-
-
 }
-
 
 updateTime();
 setInterval(updateTime, 1000);
 
-
 let currentCitiesElement = document.querySelector("#cities");
 currentCitiesElement.addEventListener("change", updateCity );
+
 
 
 
