0805d1d6dc496796bf9426cd3162c3a4d9d5322d
Joh-Hanna <Johannaruhm@web.de>
2024-06-14 23:38:58 +0200

    added the current location

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ pragueDateElement.innerHTML = `${pragueTime.format("MMM Do YYYY")}`;
 function updateCity(event) {
     
     let cityTimezone = event.target.value;
+    if (cityTimezone === "current") {
+        cityTimezone = moment.tz.guess ()
+    }
     let cityName = cityTimezone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimezone);
     let cityElement = document.querySelector("#current-shown-cities");
